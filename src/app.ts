88acd2072ff8daef715bef9822e530c1a24b0dd9
giancarvalho <giandcarvalho@gmail.com>
2021-12-16 16:29:23 -0300

    delete user files

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,12 @@ import "reflect-metadata";
 import handleErrors from "./middlewares/handleError.middeware";
 import connectDatabase from "./database";
 import examRoute from "./routes/examRoute";
-import * as userController from "./controllers/userConroller";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/exams", examRoute);
-
-app.get("/users", userController.getUsers);
-
-app.get("/health", async (req, res) => {
-    res.sendStatus(200);
-});
 
 app.use(handleErrors);
 

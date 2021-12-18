2b702598d6b75e1ef179ccee99de9f3f6b1fe773
giancarvalho <giandcarvalho@gmail.com>
2021-12-18 19:13:27 -0300

    add integration test to GET /teachers endpoint

--- tests/integration/teachers.test.ts
+++ tests/integration/teachers.test.ts
@@ -6,16 +6,18 @@ import app, { init } from "../../src/app";
 import Subject from "../../src/entities/Subject";
 import { iSubjectTeachersDB } from "../../src/protocols/subjects.interface";
 
+beforeAll(async () => {
+    await init();
+});
+
 afterAll(async () => {
     await getConnection().close();
 });
 
-describe("GET /users", () => {
+describe("GET /teachers/:subjectId", () => {
     let subjects: iSubjectTeachersDB[];
 
     beforeAll(async () => {
-        await init();
-
         subjects = await getRepository(Subject).find();
     });
 
@@ -25,5 +27,21 @@ describe("GET /users", () => {
         const response = await supertest(app).get(`/teachers/${subject.id}`);
 
         expect(response.body).toHaveProperty("teachers");
+    });
+});
+
+describe("GET /teachers", () => {
+    let subjects: iSubjectTeachersDB[];
+
+    beforeAll(async () => {
+        subjects = await getRepository(Subject).find();
+    });
+
+    it("should get and array of teachers containing name and examsRegistered", async () => {
+        const response = await supertest(app).get(`/teachers`);
+
+        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body[0]).toHaveProperty("name");
+        expect(response.body[0]).toHaveProperty("examsRegistered");
     });
 });

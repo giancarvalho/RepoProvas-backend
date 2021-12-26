314cdacad227622d779514068128bdd461ae1865
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-26 01:29:49 -0300

    preparando para o review

--- src/protocols/teachers.interface.ts
+++
@@ -1,6 +0,0 @@
-interface iTeacherDB {
-    id: number;
-    name: string;
-}
-
-export { iTeacherDB };
--- src/routes/teacherRoute.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as teacherController from "../controllers/teacherController";
-
-const teacherRoute = express.Router();
-
-teacherRoute.get("/:subject", teacherController.getTeachersBySubject);
-teacherRoute.get("", teacherController.getTeachers);
-
-export default teacherRoute;
